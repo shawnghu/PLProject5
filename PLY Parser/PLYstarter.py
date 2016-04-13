50ddfd8ddee831b739bff98663b579ac0a32caa4
Yiran Shen <shenyiran91@utexas.edu>
2016-04-13 16:08:50 -0500

    no message

---
+++ PLY Parser/PLYstarter.py
@@ -0,0 +1,64 @@
+tokens = ('PID', 'INTEGER')
+literals = ['.', ':' ]
+
+# Tokens
+t_PID  = r'^.*PID.*$'
+
+
+def t_INTEGER(t):
+    r'\d+'
+    try:
+        t.value = int(t.value)
+    except ValueError:
+        print("Integer value too large %d", t.value)
+        t.value = 0
+    return t
+
+# Ignored characters
+t_ignore = " \r"
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += t.value.count("\n")
+
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+# Build the lexer
+import ply.lex as lex   # ply.lex comes from the ply folder in the PLY download.
+lexer = lex.lex()
+
+# Parsing rules
+
+global time_step
+time_step = 0
+
+def p_start(t):
+    '''start : PID
+             | empty
+    '''
+    print "Saw :", t[1]
+
+
+def p_empty(t):
+    'empty : '
+    pass
+
+def p_error(t):
+    if t == None:
+        print("Syntax error at '%s'" % t)
+    else:
+        print("Syntax error at '%s'" % t.value)
+
+import ply.yacc as yacc   # ply.yacc comes from the ply folder in the PLY download.
+parser = yacc.yacc()
+
+while True:
+    try:
+        s = raw_input('')
+    except EOFError:
+        break
+    parser.parse(s)
+
+# To run the parser do the following in a terminal window: echo "Header1 is this~Header2 and that~~Data 1.0~Data 2.0" | tr "~" "\n" | grep -v '^\s*$' | python PLYstarter.py | sed "s/_~_/ which is a float./"
